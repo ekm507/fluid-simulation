0c6df94faf7f2a7d3f6f1fb4ee6c8dee08bfad07
erfan <ekm507@chmail.ir>
2017-01-12 18:49:43 +0330

    huge bug fixes

--- code/types.h
+++
@@ -1,9 +0,0 @@
-//code to set basic types used in program.
-
-#define vary_type double
-// variables which may vary so much!
-#define position_type double
-// position type (x or y)
-#define friction_type unsigned double
-//type for friction coefficient.
- 
