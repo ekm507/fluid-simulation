d7431761dfd14cd6c4eaba9a122e097933bcb50f
erfan <ekm507@chmail.ir>
2017-01-12 18:50:35 +0330

    huge bug fixes 22

---
+++ code/point.hpp
@@ -0,0 +1,39 @@
+//define point properties and functuins.
+
+#include "types.hpp"
+
+class point
+{
+	public:
+		position_type x;
+		position_type y;
+//TODO		point point(position_type, position_type);
+};
+
+/*point point::point(position_type a, position_type b)
+{
+	x=a;
+	y=b;
+	return self;
+}*/
+
+vary_type point_dot_product(point a, point b)
+{
+	return a.x*b.x + a.y*b.y;
+}
+
+point point_minus(point a, point b)
+{
+	point resault;
+	resault.x=a.x-b.x;
+	resault.y=a.y-b.y;
+}
+
+point point_numeral_product(vary_type n, point p)
+{
+	point resault;
+	resault.x=n*p.x;
+	resault.y=n*p.y;
+	return resault;
+}
+
---
+++ code/types.hpp
@@ -0,0 +1,7 @@
+//code to set basic types used in program.
+
+#define vary_type double
+// variables which may vary so much!
+#define position_type double
+// position type (x or y)
+ 
